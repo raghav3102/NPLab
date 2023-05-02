6c3fa6e41c84e2c579f6e100df885b72669dda1b
raghav3102 <107983742+raghav3102@users.noreply.github.com>
2023-05-02 19:28:31 +0530

    Update and rename LBA.c to LBA.cpp

---
+++ LBA.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int no_of_queries, storage, output_pkt_size;
+	int input_pkt_size, bucket_size, size_left;
+	storage = 0;
+	no_of_queries = 4;
+	bucket_size = 10;
+	input_pkt_size = 4;
+	output_pkt_size = 1;
+	for (int i = 0; i < no_of_queries; i++) 
+	{
+		size_left = bucket_size - storage;
+		if (input_pkt_size <= size_left) {
+			storage += input_pkt_size;
+		}
+		else {
+			printf("Packet loss = %d\n", input_pkt_size);
+		}
+		printf("Buffer size= %d out of bucket size= %d\n",
+			storage, bucket_size);
+		storage -= output_pkt_size;
+	}
+	return 0;
+}
