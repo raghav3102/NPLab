c9cd8729a21bb45e79e4e2abecbbc0a559fcdc21
raghav3102 <107983742+raghav3102@users.noreply.github.com>
2023-07-05 13:40:42 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,27 +1,25 @@
-// C Program to implement Congestion control using Leaky Bucket algorithm
-
-#include <stdio.h>
-void main()
-{
-	int no_of_queries, storage, output_pkt_size;
-	int input_pkt_size, bucket_size, size_left;
-	storage = 0;
-	no_of_queries = 4;
-	bucket_size = 10;
-	input_pkt_size = 4;
-	output_pkt_size = 1;
-	for (int i = 0; i < no_of_queries; i++) 
-	{
-		size_left = bucket_size - storage;
-		if (input_pkt_size <= size_left) {
-			storage += input_pkt_size;
-		}
-		else {
-			printf("Packet loss = %d\n", input_pkt_size);
-		}
-		printf("Buffer size= %d out of bucket size= %d\n",
-			storage, bucket_size);
-		storage -= output_pkt_size;
-	}
-	return 0;
+#include<stdio.h>
+int main(){
+    int ip,op,bs,sto,n,sl;
+    sto = 0;
+    printf("Enter the number of queries: \n");
+    scanf("%d",&n);
+    printf("Enter the input packet size: \n");
+    scanf("%d",&ip);
+    printf("Enter the output packet size: \n");
+    scanf("%d",&op);
+    printf("Enter the bucket size: \n");
+    scanf("%d",&bs);
+    for(int i=0;i<n;i++){
+        sl = bs - sto;
+        if(ip <= sl){
+            sto += ip;
+        }
+        else{
+            printf("Packet loss: %d \n",ip);
+        }
+        printf("Buffer size %d out of bucket size %d \n",sto,bs);
+        sto -= op;
+    }
+    return 0;
 }
