083ea88438aba825e2378a623cc26c75d19e1d13
raghav3102 <107983742+raghav3102@users.noreply.github.com>
2023-07-05 12:49:22 +0530

    Update 1b.c

--- 1b.c
+++ 1b.c
@@ -1,35 +1,43 @@
-//C Program to implement Bit Stuffing, encoding and decoding concept in DDL
-
 #include<stdio.h>
 #include<string.h>
-void main()
-{
-    int n, i;
-    printf("Enter size of frame");
-    scanf("%d",&n);
-    int frame[n];
-    printf("Enter the frame\n");
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&frame[i]);
-    }
-    int count=0;
-    for(i=0;i<n;i++)
-    {
-        if(count==5&&frame[i]==1)
-        {
-            printf("01");
-            count=1;
-        }
-        else if(count<5&&frame[i]==1)
-        {
-            count++;
-            printf("1");
-        }
-        else if(frame[i]==0)
-        {
-            printf("0");
-            count=0;
+char frame[100];
+void encode(){
+    int i,j,len,count;
+    len = strlen(frame);
+    for(i=0,count=0;i<len;i++){
+        if(frame[i] == '1')
+        count++;
+        else
+        count = 0;
+        if(count == 5){
+            for(j=len;j>i;j--){
+                frame[j] = frame[j-1];
+            }
+            frame[i+1] = '0';
+            count = 0;
+            len++;
         }
     }
+    printf("Encoded frame after bitstuffing: %s \n",frame);
+}
+void decode(){
+    int i,j,len;
+    char f2[100];
+    len = strlen(frame);
+    for(i=0,j=0;i<len;i++){
+        if(frame[i] == '1')
+        f2[j++] = frame[i];
+        else if(frame[i] == '0' && frame[i-1] == '1' && frame[i-2] == '1' && frame[i-3] == '1' && frame[i-4] == '1' && frame[i-5] == '1')
+        continue;
+        else
+        f2[j++] = frame[i];
+    }
+    f2[j] = '\0';
+    printf("Frame after decoding: %s \n",f2);
+}
+int main(){
+    printf("Enter the frame: \n");
+    scanf("%s",frame);
+    encode();
+    decode();
 }
