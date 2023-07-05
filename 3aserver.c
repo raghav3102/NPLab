1a6f8edbd11000ece29fb544a116df37a995f05e
raghav3102 <107983742+raghav3102@users.noreply.github.com>
2023-07-05 12:47:54 +0530

    Update 3aserver.c

--- 3aserver.c
+++ 3aserver.c
@@ -3,60 +3,74 @@
 #include<string.h>
 #include<netdb.h>
 #include<netinet/in.h>
-#include<sys/types.h>
 #include<sys/socket.h>
+#include<sys/types.h>
 #include<arpa/inet.h>
 #include<unistd.h>
 #define MAX 80
 #define PORT 8080
 #define SA struct sockaddr
-void func(int sockfd){
+void func(int connectID){
 	char buffer[MAX];
 	int n;
 	for(;;){
-		bzero(buffer,sizeof(buffer));
-		printf("Enter the string: \n");
+		bzero(buffer,MAX);
+		read(connectID,buffer,sizeof(buffer));
+		printf("From Client: %s \n To Client: \n",buffer);
+		bzero(buffer,size);
 		n = 0;
-		while((buffer[n++]=getchar()) != 0);
-		write(sockfd,buffer,sizeof(buffer));
-		bzero(buffer,sizeof(buffer));
-		read(sockfd,buffer,sizeof(buffer));
-		printf("From the server: %s \n",buffer);
-		if((strncmp("exit",buffer,4)) != 0){
-			printf("Client closing \n");
+		while((buffer[n++]=getchar())!='\n');
+		write(connectID,buffer,sizeof(buffer));
+		if(strncmp("exit",buffer,4)==0){
+			printf("Server Exit \n");
 			break;
 		}
 	}
 }
+	
 int main(){
-	int sockfd;
-	struct socketAddress_in clientAddress;
-	sockfd = socket(AF_INET,SOCK_STREAM,0);
+	int sockfd, length, connectID;
+	struct socketAddress_in serverAddress, client;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if(sockfd == -1){
-		printf("Socket not created \n");
+		printf("Socket not created");
 		exit(0);
 	}
 	else{
-		printf("Socket created sucessfully \n");
+		printf("Socket successfully created \n");
 	}
-	bzero(&clientAddress,sizeof(clientAddress));
-	clientAddress.sin_family = AF_INET;
-	clientAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
-	clientAddress.sin_port = htons(PORT);
-	if((connect(sockfd,(SA *)&clientAddress,sizeof(clientAddress))) != 0){
-		printf("Connection unsuccessful \n");
+	bzero(&serverAddress, sizeof(serverAddress));
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddress.sin_port = htons(PORT);
+	if(bind(sockfd,(SA *)&serverAddress,sizeof(serverAddress)) != 0){
+		printf("Socket binding unsuccessful \n");
 		exit(0);
 	}
-	else {
-		printf("Successfully connected \n");
+	else{
+		printf("Socket binding successful \n");
 	}
-	func(sockfd);
+	if((listen(sockfd,5))!=0){
+		printf("Server not listening \n");
+		exit(0);
+	}
+	else{
+		printf("Server listening \n");
+	}
+	length = sizeof(client);
+	connectID = accept(sockfd, (SA*)&client, &length);
+	if(connectID < 0){
+		printf("Server accept failed");
+	}
+	else {
+		printf("Server accepted the client \n");
+	}
+	func(connectID);
 	close(sockfd);
 	return 0;
 }
-	
 
 
-//To run this program in the ubuntu terminal, use the following commands:
-gcc filename.c -o client
-./client
+// To run this code in terminal, give the following command:
+gcc filename.c -o server
+./server
